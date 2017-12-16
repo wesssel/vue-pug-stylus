7ef44ce50f40566357dd69ee86ca1dd8bd601744
Wessel <wessel.vanderpal@vergleich.org>
2017-12-17 00:14:48 +0900

    cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,6 @@ module.exports = {
   plugins: ['vue'],
   rules: {
     semi: 'off',
-    // indent: ['error', 2, {'SwitchCase': 1}],
-    // 'import/no-unresolved': 'off',
-    // 'import/extensions': [2, {'js': 'never'}],
-    // 'import/prefer-default-export': 'off',
-    // 'no-underscore-dangle': [0],
-    // 'no-param-reassign': ['error', { props: false }],
   },
   parserOptions: {
     ecmaVersion: 6,
